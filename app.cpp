242d23a3d9d3e59778f31b3d70cff81b549c8440
Orion Stellaris <jonhfeel88@gmail.com>
2026-05-27 22:45:31 +0400

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -3,6 +3,6 @@
 int main() {
     int age{23};
     std::string name{"John"};
-    std::cout<< "Hello, World. I am "<<age << " years old. "<< "My name: "<<name << ". And i from USA\n";
+    std::cout<< "Hello, World. I am "<<age << " years old.\n"<< "My name: "<<name << " and i'm from USA\n";
     return 0;
 }
