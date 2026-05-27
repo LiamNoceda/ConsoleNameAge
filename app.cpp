acbc4271e0440fe5a74fcf8a385b6918b4380b75
LiamNoceda <jonhfeel88@gmail.com>
2026-05-27 22:06:29 +0400

    first commit

---
+++ app.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int age{23};
+    std::string name{"John"};
+    std::cout<<"Hello, World. I am "<<age<<" years old. "<<"My name: "<<name<<". And i from USA"<<std::endl;
+    return 0;
+}
